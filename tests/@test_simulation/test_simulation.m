753251de08738bc64ab4d6159c6bf9635af653b6
zmahi <zmahi@mathworks.com>
2022-03-04 08:28:34 +0100

    test: Create test class related to the simulation

---
+++ tests/@test_simulation/test_simulation.m
@@ -0,0 +1,12 @@
+classdef test_simulation < matlab.unittest.TestCase
+    %TEST_RUN_SIMULATION Run simulation of the demo
+    
+    properties
+        
+    end
+    
+    methods (Test)
+        run(testCase)
+    end
+end
+
