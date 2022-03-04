220a666e5053dd76747eefcd77d46994ad3cbcb4
zmahi <zmahi@mathworks.com>
2022-03-04 08:57:13 +0100

    fix: Remove ' character

--- tests/@test_simulation/runNominal.m
+++ tests/@test_simulation/runNominal.m
@@ -2,7 +2,7 @@ function runNominal(testCase)
 %RUN Simulate demo models
 
 % Prepare simulation
-in = Simulink.SimulationInput("'sldemo_fuelsys_dd");
+in = Simulink.SimulationInput("sldemo_fuelsys_dd");
 % Configure simulation
 in = in.setModelParameter("SimulationMode",'accelerator');
 in = in.setModelParameter("Profile",'on');
