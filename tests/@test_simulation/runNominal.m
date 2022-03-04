4bb55661857df7f3c291e20c90cb8dc5e4b162aa
zmahi <zmahi@mathworks.com>
2022-03-04 10:27:22 +0100

    test: Run test with and without warning

--- tests/@test_simulation/runNominal.m
+++ tests/@test_simulation/runNominal.m
@@ -1,8 +1,13 @@
-function runNominal(testCase)
+function runNominal(testCase,provokeWarning)
 %RUN Simulate demo models
 
+% Provoke model warning if asked
+if provokeWarning
+    load_system(testCase.modelName)
+    add_block("simulink/Sinks/Scope",testCase.modelName+"/MyScope");
+end
 % Prepare simulation
-in = Simulink.SimulationInput("sldemo_fuelsys_dd");
+in = Simulink.SimulationInput(testCase.modelName);
 % Configure simulation
 in = in.setModelParameter("StopTime",testCase.stopTime);
 in = in.setModelParameter("Profile",'on');
--- tests/@test_simulation/test_simulation.m
+++ tests/@test_simulation/test_simulation.m
@@ -1,12 +1,25 @@
 classdef test_simulation < matlab.unittest.TestCase
     %TEST_RUN_SIMULATION Run simulation of the demo
-    
+
+    properties (TestParameter)
+        provokeWarning = struct(...
+            "doNotProvokeWarning",false,...
+            "doProvokeWarning",true);
+    end
+
     properties
+        modelName = "sldemo_fuelsys_dd" % Model name to test
         stopTime = '100' % Simulation duration
     end
-    
+
+    methods (TestMethodTeardown)
+        function TMT(testCase)
+            bdclose(testCase.modelName)
+        end
+    end
+
     methods (Test)
-        runNominal(testCase)
+        % Run nominal simulation
+        runNominal(testCase,provokeWarning)
     end
 end
-
